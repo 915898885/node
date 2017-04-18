4640e824763e194168abb6936a2bdc243d7f9894
915898885 <915898885@qq.com>
2017-04-18 17:34:57 +0800

    web端和服务器端

---
+++ web模块/web服务器/server.js
@@ -0,0 +1,28 @@
+var http=require('http');
+var fs=require('fs');
+var url=require('url');
+//创建服务器
+http.createServer(function(request,response){
+	//解析请求，包括文件名
+	var pathname=url.parse(request.url).pathname;
+	//输出请求的文件名
+	console.log("Request for"+pathname+"received");
+	//从文件系统中读取请求的文件内容
+	fs.readFile(pathname.substr(1),function(err,data){
+		if(err){
+			console.log(err);
+			//http状态码 404 not found
+			//Content Type：text/plain
+			response.writeHead(404,{'Content-Type':'text/html'})
+		}else{
+			//http状态码200：ok
+			//Content Type：text/plain
+			response.writeHead(200,{'Content-Type':'text/html'});
+			//响应文件内容
+			response.write(data.toString());
+		}
+		//发送相应数据
+		response.end();
+	})
+}).listen(8081);
+console.log('Server running at http://127.0.0.1:8081/');
